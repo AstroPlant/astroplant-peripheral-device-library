51f0823b67c8cbc0de7eabd092b72e3a647bb8df
Beskhue <thomas@kepow.org>
2017-10-23 02:48:15 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='astroplant-sensor-library',
+      version='0.1',
+      description='AstroPlant sensor library',
+      author='AstroPlant',
+      author_email='thomas@kepow.org',
+      url='https://astroplant.io',
+      packages=['astroplant_sensor_library',],
+     )
+
