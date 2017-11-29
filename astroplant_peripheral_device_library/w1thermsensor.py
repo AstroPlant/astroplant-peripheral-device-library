51b794af7ef4d359f93eb556ffbdf2824cae6f79
Beskhue <thomas@kepow.org>
2017-11-29 20:39:10 +0100

    Add support for W1 Thermal Sensors.

---
+++ astroplant_peripheral_device_library/w1thermsensor.py
@@ -0,0 +1,25 @@
+"""
+Module wrapping around the W1ThermSensor package.
+"""
+
+import asyncio
+from astroplant_kit.peripheral import *
+
+import w1thermsensor
+
+class W1ThermSensor(Sensor):
+
+    def __init__(self, *args, sensor_type, sensor_id, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if sensor_type == "None":
+            sensor_type = None
+        if sensor_id == "None":
+            sensor_id = None
+        
+        self.w1ThermSensor = w1thermsensor.W1ThermSensor(sensor_type = sensor_type, sensor_id = sensor_id)
+        
+    async def measure(self):
+        temperature = self.w1ThermSensor.get_temperature()
+        temperature_measurement = Measurement(self, "Temperature", "Degrees Celsius", temperature)
+        return [temperature_measurement,]
