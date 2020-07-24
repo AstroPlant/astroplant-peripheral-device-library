07a9dd9b91d26c958baf809e9662fc79330e3994
Thomas Churchman <thomas@kepow.org>
2020-07-24 17:08:38 +0200

    Change expected Pwm command schema
    Add specific Fans class that tries to ensure fans do not stall with
    power applied

--- astroplant_peripheral_device_library/pwm.py
+++ astroplant_peripheral_device_library/pwm.py
@@ -11,7 +11,25 @@ class Pwm(Actuator):
         for pin in self.pins:
             self.pi.set_PWM_range(pin, 100)
 
+    def _intensity_transform(self, intensity):
+        return intensity
+
     async def do(self, command):
-        intensity = command
-        for pin in self.pins:
-            self.pi.set_PWM_dutycycle(pin, intensity)
+        if "intensity" in command:
+            intensity = command["intensity"]
+            for pin in self.pins:
+                self.pi.set_PWM_dutycycle(pin, self._intensity_transform(intensity))
+
+
+class Fans(Pwm):
+    def _intensity_transform(self, intensity):
+        """
+        Clamp intensity to values where the fans are either off or will
+        actually likely rotate.
+        """
+        # TODO make this configurable
+        MIN_ROTATE_INTENSITY = 50.0
+        if intensity < MIN_ROTATE_INTENSITY / 2:
+            return 0.0
+        else:
+            return max(MIN_ROTATE_INTENSITY, intensity)
