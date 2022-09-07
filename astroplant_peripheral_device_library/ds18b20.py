9e03f11484257858807e6ed22b39793b9ceae837
Thomas Churchman <thomas@kepow.org>
2022-09-07 17:46:20 +0200

    ds18b20: handle exceptions, perform setup in blocking thread

--- astroplant_peripheral_device_library/ds18b20.py
+++ astroplant_peripheral_device_library/ds18b20.py
@@ -3,7 +3,11 @@ Module wrapping around the W1ThermSensor package.
 """
 
 import trio
-from astroplant_kit.peripheral import Sensor
+from astroplant_kit.peripheral import (
+    Sensor,
+    FatalPeripheralError,
+    TemporaryPeripheralError,
+)
 
 import w1thermsensor
 
@@ -15,19 +19,31 @@ class Ds18b20(Sensor):
         self.measurement_interval = configuration["intervals"]["measurementInterval"]
         self.aggregate_interval = configuration["intervals"]["aggregateInterval"]
 
-        sensor_type = (
+        self.sensor_type = (
             configuration["sensorType"] if "sensorType" in configuration else None
         )
-        sensor_id = configuration["sensorId"] if "sensorId" in configuration else None
-
-        self.w1ThermSensor = w1thermsensor.W1ThermSensor(
-            sensor_type=sensor_type, sensor_id=sensor_id
+        self.sensor_id = (
+            configuration["sensorId"] if "sensorId" in configuration else None
         )
+
+    async def set_up(self):
+        def _set_up():
+            return w1thermsensor.W1ThermSensor(
+                sensor_type=self.sensor_type, sensor_id=self.sensor_id
+            )
+
+        try:
+            self.sensor = await trio.to_thread.run_sync(_set_up)
+        except Exception as e:
+            raise FatalPeripheralError("could not set up sensor") from e
 
     async def measure(self):
         # w1thermsensor's get_temperature is blocking, and quite slow. Run it
         # in a thread and asynchronously await the result.
-        temperature = await trio.to_thread.run_sync(self.w1ThermSensor.get_temperature)
+        try:
+            temperature = await trio.to_thread.run_sync(self.sensor.get_temperature)
+        except Exception as e:
+            raise TemporaryPeripheralError("failed to read from sensor") from e
 
         temperature_measurement = self.create_raw_measurement(
             "Temperature", "Degrees Celsius", temperature
