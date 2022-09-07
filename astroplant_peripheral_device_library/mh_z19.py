a2cfa2a8b1b10d1268dbc5d3f2f8ffd3f600ca96
Thomas Churchman <thomas@kepow.org>
2022-09-07 16:13:09 +0200

    mh-z19: catch and re-raise (potentially temporary) exceptions

--- astroplant_peripheral_device_library/mh_z19.py
+++ astroplant_peripheral_device_library/mh_z19.py
@@ -1,17 +1,10 @@
 # http://eleparts.co.kr/data/design/product_file/SENSOR/gas/MH-Z19_CO2%20Manual%20V2.pdf
 # http://qiita.com/UedaTakeyuki/items/c5226960a7328155635f
 import serial
-from astroplant_kit.peripheral import Sensor
+from astroplant_kit.peripheral import Sensor, TemporaryPeripheralError
 
 
 class MhZ19(Sensor):
-
-    #: Amount of time in seconds to wait between making measurements
-    TIME_SLEEP_BETWEEN_MEASUREMENTS = 1.0
-
-    #: Amount of time in seconds over which measurements are reduced before publishing them for storage
-    TIME_REDUCE_MEASUREMENTS = 300.0
-
     def __init__(self, *args, configuration):
         super().__init__(*args)
 
@@ -34,12 +27,19 @@ class MhZ19(Sensor):
 
     async def measure(self):
         command = "\xff\x01\x86\x00\x00\x00\x00\x00\x79"
-        result = self.serial.write(bytes(command, "latin1"))
 
-        s = self.serial.read(9)
-        s = s.decode("latin1")
+        try:
+            self.serial.write(bytes(command, "latin1"))
+        except Exception as e:
+            raise TemporaryPeripheralError("could not write to sensor") from e
 
-        co2_concentration = ord(s[2]) * 256 + ord(s[3])
+        try:
+            s = self.serial.read(9)
+            s = s.decode("latin1")
+            co2_concentration = ord(s[2]) * 256 + ord(s[3])
+        except Exception as e:
+            raise TemporaryPeripheralError("could not read from sensor") from e
+
         measurement = self.create_raw_measurement(
             "Concentration", "Parts per million", co2_concentration
         )
