AstroPlant/astroplant-peripheral-device-library

commit bb7e314bf8d0e53775210e94cebc7d3b100524c5

dht22: improve threading behavior

astroplant_peripheral_device_library/dht22.py
@@ -273,15 +273,12 @@ class Dht22(Sensor):
         successful_message_count_before = self.dht22.successful_message()
 
         # Trigger a new reading in a separate thread (timing is important for the DHT22)
-        thread = threading.Thread(target=self.dht22.trigger)
-        thread.daemon = True
-        thread.start()
-        await trio.sleep(2.0)
+        try:
+            await trio.to_thread.run_sync(self.dht22.trigger)
+        except Exception as e:
+            raise TemporaryPeripheralError("failed to read from sensor (DHT22)") from e
 
         # See if there has been a new successful reading
-        # TODO: we have a mild race condition here... we should use a lock
-        # that is acquired here, as well as in DHT22._cb in the "# Is
-        # checksum ok?"- block.
         if self.dht22.successful_message() > successful_message_count_before:
             temperature = self.dht22.temperature()
             humidity = self.dht22.humidity()

commit fc2d2539786a1a3f50d2b1cc689b442c1f36fcde

dht22: wait for successful measurement
Fixes regression partially introduced in bb7e314bf8d0e53775210e94cebc7d3b100524c5

astroplant_peripheral_device_library/dht22.py
@@ -3,6 +3,7 @@ Based on:
 https://github.com/joan2937/pigpio/blob/master/EXAMPLES/Python/DHT22_AM2302_SENSOR/DHT22.py
 """
 
+import threading
 import time
 
 import pigpio
@@ -63,6 +64,7 @@ class _DHT22:
         self.powered = True
 
         self.cb = None
+        self.msg_event = threading.Event()
 
         self.suc_M = 0  # Successful message count.
         self.bad_CS = 0  # Bad checksum count.
@@ -137,6 +139,7 @@ class _DHT22:
                         self.tov = time.time()
 
                         self.suc_M += 1
+                        self.msg_event.set()
                         if self.LED is not None:
                             self.pi.write(self.LED, 0)
 
@@ -234,10 +237,15 @@ class _DHT22:
             if self.LED is not None:
                 self.pi.write(self.LED, 1)
 
+            self.msg_event.clear()
+
             self.pi.write(self.gpio, pigpio.LOW)
             time.sleep(0.017)  # 17 ms
             self.pi.set_mode(self.gpio, pigpio.INPUT)
             self.pi.set_watchdog(self.gpio, 200)
+
+            # Wait for a successful message
+            self.msg_event.wait(timeout=5.0)
 
     def cancel(self):
         """Cancel the DHT22 sensor."""